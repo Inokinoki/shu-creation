fdb667ab195efe2f2a30720e223811b7491d5a98
Inokinoki <veyx.shaw@gmail.com>
2016-08-18 18:21:07 +0000

    Rebuild database.php in OO.

--- api/database.php
+++ api/database.php
@@ -1,11 +1,41 @@
 <?php
-	$database = mysql_connect("localhost","root","creation");
-	if($database){
-		mysql_query("Set names utf8", $database);
-		$db = mysql_select_db("creation", $database);
-		if(!$db)
+class Database{
+	private $db_user	= 	"root";
+	private $db_pwd		= 	"creation";
+	private $db_host 	=	"localhost";
+	private $db_port 	=	"3306";
+	private $db 		= 	null;
+
+	function connect(){
+		if($this->db == null){
+			$this->db = mysql_connect($this->db_host, $this->db_user, $this->db_pwd);
+			if(!$this->db){
 				die(mysql_error());
-	} else {
-		die(mysql_error());
+			}
+			mysql_query("SET names utf8", $db);
+			// Limit in creation database.
+			mysql_select_db("creation", $db);
+		}
 	}
+
+	function query($db_query_expression){
+		return mysql_query($db_query_expression, $db);
+	}
+
+	function count($key, $value, $table){
+			$result = mysql_query("SELECT * IN $table WHERE $key = '$value'");
+			return mysql_num_rows($result);
+	}
+
+	function exist($key, $value, $table){
+			return count($key, $value, $table)==0 ? false : true;
+	}
+
+	function __destruct() {
+		if(!$db){
+			mysql_close($db);
+		}
+	}
+}
+	$database = new Database();
 ?>
