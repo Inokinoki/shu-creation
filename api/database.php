ebad72536876ed2de741370eba750f91eb64ca35
Inokinoki <veyx.shaw@gmail.com>
2016-08-18 17:52:46 +0000

    Fix Chinese character display problem.

--- api/database.php
+++ api/database.php
@@ -1,10 +1,11 @@
 <?php
-		$database = mysql_connect("localhost","root","creation");
-		if($database){
-				$db = mysql_select_db("creation", $database);
-				if(!$db)
-						die(mysql_error());
-		} else {
+	$database = mysql_connect("localhost","root","creation");
+	if($database){
+		mysql_query("Set names utf8", $database);
+		$db = mysql_select_db("creation", $database);
+		if(!$db)
 				die(mysql_error());
-		}
+	} else {
+		die(mysql_error());
+	}
 ?>
