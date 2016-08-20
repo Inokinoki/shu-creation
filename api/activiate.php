afb0baf751999453db3afb7c5e9cbcd78cba9eb0
Inoki <inoki@Inoki.centos>
2016-08-20 19:28:46 +0800

    Finish activiate.php function.

---
+++ api/activiate.php
@@ -0,0 +1,40 @@
+<?php
+/*
+ * Use this api to activiate user's account
+ * Login link: http://passport.lehu.shu.edu.cn/LoginDo.aspx?action=1
+ */
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    
+    $url = "http://passport.lehu.shu.edu.cn/LoginDo.aspx?action=1";
+    $post_data = array ("username" => $username,"password" => $password);
+    $passprt_curl = curl_init();
+    curl_setopt($passprt_curl, CURLOPT_URL, $url);
+    curl_setopt($passprt_curl, CURLOPT_RETURNTRANSFER, 1);
+    // Post
+    curl_setopt($passprt_curl, CURLOPT_POST, 1);
+    curl_setopt($passprt_curl, CURLOPT_POSTFIELDS, $post_data);
+    $passport_output = curl_exec($passprt_curl);
+    curl_close($passprt_curl);
+    // Handle 
+    $passport_result = $passport_output.trim("|");
+    if ($passport_result[0]==0){
+        // Save user info into database
+        require_once("./database.php");
+        $password = md5($password);
+        if ($database->exist("username", $username, "accounts")){
+            // Activiate to reset password.
+            $database->query("UPDATE accounts SET password = '$password', uuid = '' 
+                WHERE username = '$username'");
+        } else {
+            // Activiate to activiate.
+            $database->query("INSERT INTO accounts
+                (username, password, nickname, name, _id, uuid) VALUES 
+                ('$username', '$password', '$username', '', null, '')");
+        }
+        echo 0;
+    } else {
+        // Wrong password.
+        echo 1;
+    }
+?>
