bb00a51f8209044ac1943a0e5a5489c23f03a107
Inoki <inoki@Inoki.centos>
2016-08-20 19:48:34 +0800

    User update info api

---
+++ api/user_update.php
@@ -0,0 +1,32 @@
+<?php
+    $uuid = $_COOKIE["creation_uuid"];  // Get uuid.
+
+    if (!empty($uuid)){
+        require_once("./database.php");
+        if ($database->exist("uuid", $uuid, "accounts")){
+            $name = $_POST["name"];
+            $nickname = $_POST["nickname"];
+            $password = $_POST["password"];
+            $return_code = 0;
+            if (!empty($name)){
+                $database->query("UPDATE accounts SET name = '$name' WHERE uuid = '$uuid'");
+            }
+            if (!empty($nickname)){
+                $database->query("UPDATE accounts SET nickname = '$nickname' WHERE uuid = '$uuid'");
+            }
+            if (!empty($password)){
+                $database->query("UPDATE accounts SET password = '$password' WHERE uuid = '$uuid'");
+                $database->query("UPDATE accounts SET uuid = '' WHERE uuid = '$uuid'");
+                $return_code = 4;
+            }
+            echo $return_code;
+        } else {
+            // Maybe login state out of time
+            echo 2;
+        }
+    } else {
+        // No uuid -- ca veut dire not logged
+        echo 1;
+    }
+
+?>
