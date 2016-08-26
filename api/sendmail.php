dc07bb8e7c00a362e80ca4ce343e0d7247c14598
Inoki <inoki@Inoki.centos>
2016-08-27 00:28:51 +0800

    Add send mail test page

--- api/sendmail.php
+++ api/sendmail.php
@@ -1,29 +1,33 @@
 <?php
-    
     // Get param
-
     $mailto         =   $_POST["mail_to"];//发送给谁   
     $mailsubject    =   $_POST["mail_subject"];//邮件主题   
     $mailbody       =   $_POST["mail_body"];//邮件内容  
-
     if (!empty($mailto)) {
         if (!empty($mailsubject)) {
             if (!empty($mailbody)) {
-                // TO-DO require level >=3;
-
-                // Send email to target user, permission level >= 3
-                require_once("./smtp.php");  
-                ##########################################   
-                $smtpserver = "smtp.163.com";//SMTP服务器   
-                $smtpserverport = 25;//SMTP服务器端口   
-                $smtpusermail = "zhongouchuanghuan@163.com";//SMTP服务器的用户邮箱   
-                $smtpuser = "zhongouchuanghuan@163.com";//SMTP服务器的用户帐号   
-                $smtppass = "creation2016shu";//SMTP服务器的用户密码   
-                $mailtype = "HTML";//邮件格式（HTML/TXT）,TXT为文本邮件   
-                ##########################################   
-                $smtp = new SMTP($smtpserver,$smtpserverport,true,$smtpuser,$smtppass);   
-                $smtp->debug = true;//是否显示发送的调试信息   
-                $smtp->sendmail($mailto, $smtpusermail, $mailsubject, $mailbody, $mailtype);
+                // Require level >=3;
+                require_once("./level.php");
+                $level = new LevelSystem(3, $_COOKIE["creation_uuid"]);
+                if ($level->validate()){
+                    // Send email to target user, permission level >= 3
+                    require_once("./smtp.php");  
+                    ##########################################   
+                    $smtpserver = "smtp.163.com";//SMTP server   
+                    $smtpserverport = 25;//SMTP port 
+                    $smtpusermail = "zhongouchuanghuan@163.com";//SMTP user mail  
+                    $smtpuser = "zhongouchuanghuan@163.com";//SMTP account
+                    $smtppass = "creation2016shu";//SMTP password
+                    $mailtype = "HTML";//Mail format
+                    ##########################################   
+                    $smtp = new SMTP($smtpserver,$smtpserverport,true,$smtpuser,$smtppass);   
+                    $smtp->debug = false;//是否显示发送的调试信息   
+                    $smtp->sendmail($mailto, $smtpusermail, $mailsubject, $mailbody, $mailtype);
+                    echo "0";
+                } else {
+                    // Permission denied.
+                    echo "1|0";
+                }
             } else {
                 echo "1|3";
             }
