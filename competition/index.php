c2da220453c39c8eed7bb027c3b32a60c652b80c
Inokinoki <veyx.shaw@gmail.com>
2016-12-12 23:20:52 +0800

    Add competition page

---
+++ competition/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="author" content="Inoki Shaw"/>
+        <meta name="keywords" content="Lego;Robot;Creation;Shanghai University"/>
+        <meta name="description" content="Shanghai University Creation Club Official Website"/>
+
+        <title>上海大学创幻社</title>
+        <link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <script src="http://cdn.bootcss.com/jquery/1.11.3/jquery.min.js"></script>
+        <script src="http://cdn.bootcss.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+        <script src="./js/md5.js"></script>
+        <script src="./js/validate.js"></script>
+        <script src="./js/account.js"></script>
+
+        <!--[if lt IE 9]>
+        <script src="http://cdn.bootcss.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="http://cdn.bootcss.com/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+			<?php
+				require_once("./header.php");
+			?>
+        <div class="container" role="main">
+            <div class="panel panel-default">
+                <div class="panel-heading"><h2>第七届 “中欧杯” 创幻机器人大赛报名表</h2></div>
+                    <div class="panel-body">
+                        <div class="col-lg-7 col-md-7 col-sm-12 col-xs-12" id="join_form">
+                            <p>
+								欢迎您报名参加 第七届 “中欧杯” 创幻机器人大赛。<br/>
+								<br/>
+								请于<span class="text-danger">2016年12月25日 18：00 前</span>将报名表发送至<span class="text-primary">zhongouchuanghuan@163.com</span><br/>
+								<br/>
+								点击此处
+								<a type="button" class="btn btn-default" href="./第七届中欧杯创幻机器人大赛报名表.docx">下载</a>
+								报名表。
+							</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
